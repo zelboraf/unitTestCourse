a183802a937dcbccb539dba9a9e3e5d8da80219e
zelboraf <uminski.krzysztof@gmail.com>
2020-10-14 23:08:31 +0200

    added StringCalculator with unit tests

---
+++ src/main/java/module1/StringCalculator.java
@@ -0,0 +1,19 @@
+package module1;
+
+public class StringCalculator {
+
+    public int add(String input) {
+        if (input.isEmpty()) {
+            return 0;
+        }
+        String[] numbers = input.split(",");
+        int result = 0;
+
+        for (String s : numbers) {
+            result += Integer.parseInt(s);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/test/java/module1/StringCalculatorTest.java
@@ -0,0 +1,42 @@
+package module1;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+    private static StringCalculator calculator;
+
+    @BeforeAll
+    static void initTest() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    void shouldReturnZeroWhenEmptyInputGiven() {
+        assertEquals(0, calculator.add(""));
+        assertEquals(0, calculator.add(",,,"));
+    }
+
+    @Test
+    void shouldReturnNumberWhenNumberGiven() {
+        assertEquals(0, calculator.add("0"));
+        assertEquals(-1, calculator.add("-1"));
+        assertEquals(2, calculator.add("2"));
+    }
+
+    @Test
+    void shouldSumOfTwoNumbersWhenTwoNumbersGiven() {
+        assertEquals(5, calculator.add("2,3"));
+        assertEquals(0, calculator.add("1,-1"));
+        assertEquals(0, calculator.add("0,0"));
+    }
+
+    @Test
+    void shouldSumOfThreeNumbersWhenThreeNumbersGiven() {
+        assertEquals(9, calculator.add("6,2,1"));
+        assertEquals(0, calculator.add("6,-5,-1"));
+    }
+
+}
