58d1156167cb5d2d626140a5e797975cabbbfde9
zelboraf <uminski.krzysztof@gmail.com>
2020-12-30 17:02:07 +0100

    added logger to vatService

--- src/main/java/module2_vatservice/v2/VatService.java
+++ src/main/java/module2_vatservice/v2/VatService.java
@@ -1,25 +1,33 @@
 package module2_vatservice.v2;
 
+import org.apache.log4j.Logger;
+
 class VatService {
     VatProvider vatProvider;
-
+    final static Logger logger = Logger.getLogger(VatService.class);
 
     public VatService(VatProvider vatProvider) {
         this.vatProvider = vatProvider;
     }
 
     public double getGrossPriceForDefaultVat(Product product) throws IncorrectVatException {
-        return calculateGrossPrice(product.getNetPrice(), vatProvider.getDefaultVat());
+        logger.info("Get price for default VAT");
+        return calculateGrossPrice(product, vatProvider.getDefaultVat());
     }
 
     public double getGrossPriceForCustomVat(Product product) throws IncorrectVatException {
-        double vatValue = vatProvider.getVatForType(product.getType());
-        return calculateGrossPrice(product.getNetPrice(), vatValue);
+        logger.info("Get price for custom VAT");
+        return calculateGrossPrice(product, vatProvider.getVatForType(product.getType()));
     }
 
-    private double calculateGrossPrice(double netPrice, double vatValue) throws IncorrectVatException {
+    private double calculateGrossPrice(Product product, double vatValue) throws IncorrectVatException {
+        double netPrice = product.getNetPrice();
+        logger.info(String.format("Calculate gross price for netPrice: %.2f, vatValue: %.2f, type: %s",
+                netPrice, vatValue, product.getType()));
         if(vatValue > 1){
-            throw new IncorrectVatException("VAT can't be higher than 100%");
+            String errorMessage = "VAT can't be higher than 100%";
+            logger.error(errorMessage);
+            throw new IncorrectVatException(errorMessage);
         }
         return netPrice * (1 + vatValue);
     }
